760dca521118e3e18af80bb0d12c3f9504f5c20f
AyushWaghe <144901247+AyushWaghe@users.noreply.github.com>
2024-02-23 17:25:49 +0530

    Update studentLogin.js

--- client/src/Auth/studentLogin.js
+++ client/src/Auth/studentLogin.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Link,useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/auth.js';
 import { toast } from 'react-toastify';
